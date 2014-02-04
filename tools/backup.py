58c9d06a6433792b957238b917e8eacaccbdff26
quadnix <quadnix1@gmail.com>
2014-02-04 11:56:25 -0500

    restructure ALL the things
    removed builds scripts - to be rebuilt 96% less stupidly

--- tools/backup.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env python
-import os
-name = raw_input("Name for tarred sources [omit .tar.gz]: ")
-os.system("cd ..")
-os.system("tar -zcvf backups/" + name + ".tar.gz source")
-os.system("cd tools")
