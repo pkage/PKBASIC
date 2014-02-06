2c70a3a899d0a814fa15217950dbaf8c6400b9bc
quadnix <quadnix1@gmail.com>
2014-02-06 13:52:36 -0500

    changed cfg

--- engine/source/extensions/testlib.cpp
+++ engine/source/extensions/testlib.cpp
@@ -3,7 +3,11 @@
 
 using namespace std;
 
-int main() {
-	
+int main(int argc, char* argv[]) {
+	if (argc != 3) {
+		cout << "invocation failed\n";
+		return 1;
+	}
+	cout << "\nvardump: " << argv[1] << "\ninvocation: " << argv[2] << "\n";
 	return 0;
 }
