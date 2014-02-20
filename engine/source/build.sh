897139cb18dca044d56e4a9b803e5154955398ec
quadnix <quadnix1@gmail.com>
2014-02-20 13:54:11 -0500

    updated building

--- engine/source/build.sh
+++ engine/source/build.sh
@@ -2,6 +2,8 @@
 
 cd ../build
 ./build.py ../source/buildconfig/testlib.cfg ../source/buildconfig/varlist.cfg
+mv ../source/extensions/varlib.o /usr/local/pkb
+mv ../source/extensions/testlib.o /usr/local/pkb
 cd ../..
 ./install.py
 cd engine/source
