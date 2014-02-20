eeee6ffe32221f721b3cae5b7b6183e24ecfed99
quadnix <quadnix1@gmail.com>
2014-02-20 10:47:55 -0500

    made build script install the interpreter into the path

--- engine/source/build.sh
+++ engine/source/build.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 
 cd ../build
-./build.py ../source/buildconfig/main.cfg ../source/buildconfig/testlib.cfg ../source/buildconfig/varlist.cfg
+./build.py ../source/buildconfig/testlib.cfg ../source/buildconfig/varlist.cfg
 cd ../..
 ./install.py
 cd engine/source
